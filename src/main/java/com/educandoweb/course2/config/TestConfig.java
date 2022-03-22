52665dc93b4472fc805bb82d6025ebfb306f4383
tiagopires79 <tiagopiresdb@gmail.com>
2022-03-22 14:45:17 -0300

    OrderItem, many-to-many association with extra attributes - Part 1

--- src/main/java/com/educandoweb/course2/config/TestConfig.java
+++ src/main/java/com/educandoweb/course2/config/TestConfig.java
@@ -71,8 +71,7 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
-		
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));		
 
 	}
 }
